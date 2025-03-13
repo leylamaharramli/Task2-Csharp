9dfddf0eb4f276398af20ee4f7eb40e3a4a324fd
leylamaharramli <leyla@example.com>
2025-03-13 21:24:10 +0400

    second commit

---
+++ task-2/Program.cs
@@ -0,0 +1,28 @@
+﻿#region
+//int num = Convert.ToInt32(Console.ReadLine());
+//int biggest_num = 0;
+
+//while ( num > 0)
+//{
+//    if ( num % 10 > biggest_num)
+//    {
+//        biggest_num = num % 10;
+//    }
+//    num = (num-num%10)/10;
+//}
+//Console.WriteLine(biggest_num);
+#endregion
+
+#region
+int num = Convert.ToInt32(Console.ReadLine());
+int power = 0;
+int eded = 1;
+
+while (eded*2 <= num)
+{
+    eded *= 2; 
+    power++;
+}
+
+Console.WriteLine(power);
+#endregion
